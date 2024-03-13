ec545191b4586bc48f4abd19afe906b9b7b9d33c
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-03-13 16:12:23 +0700

    update

---
+++ WebsiteBanHang2/WebsiteBanHang2/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebsiteBanHang2.Models;
+
+
+namespace WebsiteBanHang2.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+    }
+}
---
+++ WebsiteBanHang2/WebsiteBanHang2/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteBanHang2.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+		[Required, StringLength(100)]
+		public string? Name { get; set; }
+		[Range(0.01, 10000.00)]
+		public string? Description { get; set; }
+		public decimal Price { get; set; }
+		public string? ImageUrl { get; set; }
+		public List<ProductImage>? Images { get; set; }
+		public int CategoryId { get; set; }
+		public Category? Category { get; set; }
+	}
+}
