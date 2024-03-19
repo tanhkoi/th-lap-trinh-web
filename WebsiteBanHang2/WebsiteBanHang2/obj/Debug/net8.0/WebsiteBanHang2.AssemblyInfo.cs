ccf2ee77d01d6950689bda45345ed128ef1ee7af
Nguyen Tan Khoi <khoitony35@gmail.com>
2024-03-19 22:34:36 +0700

    push some files, that can make error occurred +D

--- WebsiteBanHang2/WebsiteBanHang2/obj/Debug/net8.0/WebsiteBanHang2.AssemblyInfo.cs
+++ WebsiteBanHang2/WebsiteBanHang2/obj/Debug/net8.0/WebsiteBanHang2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanHang2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79d432c3b573201dadc4aa928329b8959a556221")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20975acb8bacfb35f2ebdc16535e46d04b08148")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanHang2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanHang2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
